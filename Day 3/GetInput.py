265ea52289bb040e8ebee6850cebfc524f4c89f7
jeppe <jeppemlv@gmail.com>
2021-12-20 17:26:25 +0100

    token

---
+++ Day 3/GetInput.py
@@ -0,0 +1,24 @@
+from pip._vendor import requests
+import sys
+import re
+
+
+try:
+    with open(".env", "r") as file:
+        TOKEN = file.readline()
+except:
+    print("no token, shutting down")
+    exit()
+
+path = sys.path[0]+"\input.txt"
+m = re.search(r'\d+$', sys.path[0])
+day = int(m.group()) if m else 0
+URL = f"https://adventofcode.com/2021/day/{day}/input"
+token = {"session": TOKEN}
+USER_AGENT = {"User-Agent": "advent-of-code-data v{}".format(1)}
+
+response = requests.get(url=URL, cookies=token, headers=USER_AGENT)
+data = response.text
+
+with open(path, "w") as f:
+    f.write(data)
